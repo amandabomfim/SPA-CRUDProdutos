f778ef2ab804debf0fa7bf2f812bc84f8263611c
amandabomfim <rm99741@fiap.com.br>
2023-09-26 19:30:55 -0300

    Adicionar Produto estilizado com css module.

--- src/routes/AdicionarProduto.jsx
+++ src/routes/AdicionarProduto.jsx
@@ -1,7 +1,7 @@
 import { ListaProdutos } from "../components/ListaProdutos";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-
+import classes from "./AdicionarProduto.module.css"
 export default function AdicionarProduto() {
   document.title = "Adicionar Produto";
   const navigate = useNavigate();
@@ -26,31 +26,31 @@ export default function AdicionarProduto() {
   };
   return (
     <>
-      <h1>Adicionar Produto</h1>
-      <form onSubmit={handleSubmit}>
+      <h1 className={classes.Centralizar}>ADICIONAR PRODUTO</h1>
+      <form onSubmit={handleSubmit} className={classes.Formulario}>
         <fieldset>
-          <legend>ADICIONAR PRODUTO</legend>
+          <legend className={classes.Negrito}>ADICIONAR PRODUTO</legend>
           <div>
-            <label htmlFor="idNome">Nome:</label>
+            <label htmlFor="idNome" className={classes.Negrito}>Nome:</label>
             <input type="text" name='nome' placeholder="Nome..." onChange={handleChange} required />
           </div>
           <div>
-            <label htmlFor="idPreco">Preço:</label>
+            <label htmlFor="idPreco" className={classes.Negrito}>Preço:</label>
             <input type="number" name='preco' placeholder="Preço..." onChange={handleChange} required />
           </div>
           <div>
-            <label htmlFor="idDesc">Descrição:</label>
+            <label htmlFor="idDesc" className={classes.Negrito}>Descrição:</label>
             <input type="text"name='desc' placeholder="Desc..." onChange={handleChange} required />
           </div>
           <div>
-            <label htmlFor="idImg">Imagem:</label>
+            <label htmlFor="idImg" className={classes.Negrito}>Imagem:</label>
             <input type="text" name='img' onChange={handleChange} placeholder="URL..." required/>
           </div>
-          <input type="submit" value="Enviar"/>
+          <input type="submit"  className={classes.Botao}  value="Enviar"/>
         </fieldset>
       </form>
-      <div>
-        <p>Nome: {produto.nome}</p>
+      <div className={classes.Negrito}>
+        <p >Nome: {produto.nome}</p>
         <p>Preço: {produto.preco}</p>
         <p>Descrição: {produto.desc}</p>
         <p>Imagem: {produto.img}</p>
